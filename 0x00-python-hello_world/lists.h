3ae568c3fe5ae7a015fffa133f1ddc54b1726b5e
Hlonilen <Hlonilen>
2023-09-04 05:47:37 -0700

    lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -5,20 +5,20 @@
 
 /**
  * struct listint_sym - singly linked list
- * @num: integer
+ * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  * for Holberton project
  */
-typedef struct listint_sym
+typedef struct listint_s
 {
-	int num;
-	struct listint_sym *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint_t *ham);
-listint_t *add_nodeint(listint_t **head, const int num);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
